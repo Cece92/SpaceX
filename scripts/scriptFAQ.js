40d5312429d0e9d2b6e35914ebe0f4899d98884a
Celia <cguyobon@live.com>
2018-07-27 15:09:48 +0200

    r
    rr

--- scripts/scriptFAQ.js
+++ scripts/scriptFAQ.js
@@ -1,9 +1,28 @@
-let question = document.querySelector(".question")
-let answer = document.querySelector("answer")
+let question1 = document.querySelector(".question1")
+let answer1 = document.querySelector(".answer1")
+let question2 = document.querySelector(".question2")
+let answer2 = document.querySelector(".answer2")
+let question3 = document.querySelector(".question3")
+let answer3 = document.querySelector(".answer3")
+let isActive = 0
 
-question.addEventListener(
-  'click'
+question1.addEventListener(
+  'click',
   function(){
-    answer.style.display = "block"
-  }
-)
+    answer1.style.display = "block"
+  }, false
+);
+
+question2.addEventListener(
+  'click',
+  function(){
+    answer2.style.display = "block"
+  }, false
+);
+
+question3.addEventListener(
+  'click',
+  function(){
+    answer3.style.display = "block"
+  }, false
+);
